7226618d80864cfe059c6106da27a78ce4e50ec2
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-06-06 14:47:49 +0300

    feat: event create

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Layout } from "./Layout";
 import dataProvider from "./dataProvider";
 import { EventList } from "./events/EventList";
 import { EventShow } from "./events/EventShow";
+import { EventCreate } from "./events/EventCreate";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
     <Resource 
         name="events" 
+        create={EventCreate}
         list={EventList}
         show={EventShow}
          />
