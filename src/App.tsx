6c223c20e40c5a3522d500dcffed86a17fdbb6d1
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-06-06 14:00:35 +0300

    chore: data provider with required methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { Admin } from "react-admin";
+import { Admin, ListGuesser, Resource } from "react-admin";
 import { Layout } from "./Layout";
+import dataProvider from "./dataProvider";
 
-export const App = () => <Admin layout={Layout}></Admin>;
+export const App = () => <Admin layout={Layout} dataProvider={dataProvider} >
+    <Resource name="events" list={ListGuesser} />
+</Admin>;
---
+++ src/dataProvider.ts
@@ -0,0 +1,63 @@
+import { DataProvider, DeleteManyParams, DeleteManyResult, fetchUtils, GetManyReferenceParams, GetManyReferenceResult, QueryFunctionContext, RaRecord, UpdateManyParams, UpdateManyResult } from "react-admin";
+
+const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:3000/api";
+const httpClient = fetchUtils.fetchJson;
+
+const dataProvider: DataProvider = {
+
+  getList: async (resource, params) => {
+    const url = `${apiUrl}/${resource}`;
+    const { json } = await httpClient(url);
+
+    return {
+      data: json,
+    };
+  },
+
+  getOne: async (resource, params) => {
+    const { json } = await httpClient(`${apiUrl}/${resource}/${params.id}`);
+    return { data: json };
+  },
+
+  getMany: async (resource, params) => {
+    const query = { filter: JSON.stringify({ id: params.ids }) };
+    const url = `${apiUrl}/${resource}?${new URLSearchParams(query)}`;
+    const { json } = await httpClient(url);
+    return { data: json };
+  },
+
+  create: async (resource, params) => {
+    const { json } = await httpClient(`${apiUrl}/${resource}`, {
+      method: "POST",
+      body: JSON.stringify(params.data),
+    });
+    return { data: json };
+  },
+
+  update: async (resource, params) => {
+    const { json } = await httpClient(`${apiUrl}/${resource}/${params.id}`, {
+      method: "PATCH",
+      body: JSON.stringify(params.data),
+    });
+    return { data: json };
+  },
+
+  delete: async (resource, params) => {
+    const { json } = await httpClient(`${apiUrl}/${resource}/${params.id}`, {
+      method: "DELETE",
+    });
+    return { data: json };
+  },
+  
+  getManyReference: function <RecordType extends RaRecord = any>(resource: string, params: GetManyReferenceParams & QueryFunctionContext): Promise<GetManyReferenceResult<RecordType>> {
+    throw new Error("Function not implemented.");
+  },
+  updateMany: function <RecordType extends RaRecord = any>(resource: string, params: UpdateManyParams): Promise<UpdateManyResult<RecordType>> {
+    throw new Error("Function not implemented.");
+  },
+  deleteMany: function <RecordType extends RaRecord = any>(resource: string, params: DeleteManyParams<RecordType>): Promise<DeleteManyResult<RecordType>> {
+    throw new Error("Function not implemented.");
+  }
+};
+
+export default dataProvider;
