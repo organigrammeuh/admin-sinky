60afe1c3d171c0709e3d4457cbc1b9dbe6b29477
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-06-06 14:26:26 +0300

    chore: date formating on the event list

--- src/events/EventList.tsx
+++ src/events/EventList.tsx
@@ -1,12 +1,16 @@
-import { DataTable, List } from "react-admin"
+import { DataTable, DateField, List } from "react-admin"
 
 export const EventList = () => (
-    <List>
+    <List >
         <DataTable>
             <DataTable.Col source="id" />
             <DataTable.Col source="title" />
-            <DataTable.Col source="startDate" />
-            <DataTable.Col source="endDate" />
+            <DataTable.Col source="startDate" >
+                <DateField source="startDate" locales="en-US" />
+            </DataTable.Col>
+            <DataTable.Col source="endDate" >
+                <DateField source="endDate" locales="en-US" />
+            </DataTable.Col>
             <DataTable.Col source="location" />
         </DataTable>
     </List>
