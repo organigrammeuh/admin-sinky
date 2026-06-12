de19ecf3b3fcf2c5c77a07c1927320877fa0fd5b
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-06-13 00:50:30 +0300

    feat: session filtering

--- src/sessions/SessionList.tsx
+++ src/sessions/SessionList.tsx
@@ -1,14 +1,23 @@
-import { List, DataTable } from "react-admin";
+import { List, DataTable, ReferenceInput, SelectInput, TextInput, DateTimeInput } from "react-admin";
+
+const sessionFilters = [
+    <TextInput source="title" label="Search by title" alwaysOn />,
+    <ReferenceInput source="event_id" reference="events" perPage={100} alwaysOn >
+        <SelectInput optionText="title" />
+    </ReferenceInput>,
+    <DateTimeInput source="start_date" label="Start after" alwaysOn  />,
+    <DateTimeInput source="e" label="End before" alwaysOn  />,
+];
 
 export const SessionList = () => (
-    <List>
+    <List filters={sessionFilters}>
         <DataTable rowClick="show">
             <DataTable.Col source="id" />
             <DataTable.Col source="title" />
             {/* <DataTable.Col source="description" /> */}
             <DataTable.Col source="startTime" />
             <DataTable.Col source="endTime" />
-            <DataTable.Col source="eventId" />
+            <DataTable.Col source="event.title" />
         </DataTable>
     </List>
 );
