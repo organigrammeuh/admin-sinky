521963e416a17fdfb2e402710114efd09cec8c4e
Tsiry <tsiryras3@gmail.com>
2026-06-27 17:45:38 +0300

    feat: add session list to the room show

--- src/rooms/RoomShow.tsx
+++ src/rooms/RoomShow.tsx
@@ -1,10 +1,15 @@
-import { Show, SimpleShowLayout, TextField } from "react-admin";
+import { ReferenceManyField, Datagrid, Show, SimpleShowLayout, TextField } from "react-admin";
 
 export const RoomShow = () => (
   <Show>
     <SimpleShowLayout>
       <TextField source="id" />
       <TextField source="name" />
+      <ReferenceManyField reference="sessions" target="roomId" label="Sessions">
+        <Datagrid bulkActionButtons={false} rowClick={(id) => `/sessions/${id}/show`}>
+          <TextField source="title" />
+        </Datagrid>
+      </ReferenceManyField>
     </SimpleShowLayout>
   </Show>
 );
