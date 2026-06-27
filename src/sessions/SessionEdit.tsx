2ec51b4851d9e67813250cc6b43694f4cde97cc8
Tsiry <tsiryras3@gmail.com>
2026-06-27 21:49:34 +0300

    feat: add style to the sessions form

--- src/sessions/SessionEdit.tsx
+++ src/sessions/SessionEdit.tsx
@@ -12,6 +12,7 @@ import {
   TextInput,
 } from "react-admin";
 import { useSearchParams } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 export const SessionEdit = () => {
   const [searchParams] = useSearchParams();
@@ -22,17 +23,20 @@ export const SessionEdit = () => {
       transform={(data) => ({ ...data, eventId })}
       redirect={eventId ? `/events/${eventId}/show` : false}
     >
-      <SimpleForm>
-        <TextInput source="title" validate={[required()]} />
-        <TextInput source="description" validate={[required()]} />
-        <DateTimeInput source="startTime" validate={[required()]} />
-        <DateTimeInput source="endTime" validate={[required()]} />
+      <SimpleForm sx={{ maxWidth: 600, p: 4 }}>
+        <Typography variant="h5" className="text-gradient" sx={{ fontWeight: "bold", mb: 3 }}>
+          Modifier la Session
+        </Typography>
+        <TextInput source="title" validate={[required()]} fullWidth sx={{ mb: 2 }} />
+        <TextInput source="description" validate={[required()]} multiline rows={4} fullWidth sx={{ mb: 2 }} />
+        <DateTimeInput source="startTime" validate={[required()]} fullWidth sx={{ mb: 2 }} />
+        <DateTimeInput source="endTime" validate={[required()]} fullWidth sx={{ mb: 2 }} />
         <ReferenceInput source="roomId" reference="rooms">
-          <SelectInput optionText="name" validate={[required()]} label="Room" />
+          <SelectInput optionText="name" validate={[required()]} label="Room" fullWidth sx={{ mb: 2 }} />
         </ReferenceInput>
-        <NumberInput source="capacity" validate={[required(), minValue(1)]} />
+        <NumberInput source="capacity" validate={[required(), minValue(1)]} fullWidth sx={{ mb: 2 }} />
         <ReferenceArrayInput source="speakerIds" reference="speakers">
-          <SelectArrayInput optionText="fullName" label="Speakers" />
+          <SelectArrayInput optionText="fullName" label="Speakers" fullWidth sx={{ mb: 2 }} />
         </ReferenceArrayInput>
       </SimpleForm>
     </Edit>
