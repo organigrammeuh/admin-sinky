d40c8cd1fa926ee3fbaba9415ce18e414a21a0b3
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-06-28 22:15:49 +0300

    feat : add proxy for local development using port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     sourcemap: mode === "development",
